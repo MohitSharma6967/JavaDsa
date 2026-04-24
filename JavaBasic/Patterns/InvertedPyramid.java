bb361a576f1e13ae2528819dbbb9092f104a8aed
MohitSharma6967 <a9939359@gmail.com>
2026-04-24 18:13:40 +0530

    Added InvertedPyramid pattern problem

---
+++ JavaBasic/Patterns/InvertedPyramid.java
@@ -0,0 +1,20 @@
+public class InvertedPyramid{
+
+    public static void InvertPyramid(int totalRows, int totalColumn){
+        for( int i = 1; i <= totalRows; i++){
+            //spaces
+            for(int j = 1; j <= totalRows-i; j++){
+                System.out.print("   ");
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print(" * ");
+            }
+
+        System.out.println();
+        }
+    }
+    
+    public static void main(String[] args){
+        InvertPyramid(4,4);
+    }
+}
