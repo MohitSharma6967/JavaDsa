98031cf622b9618c46d24079cf0046541d560a2a
MohitSharma6967 <a9939359@gmail.com>
2026-05-04 15:27:34 +0530

    More challenges until i get clear

--- JavaBasic/Arrays/challenge of chatgpt/Test.java
+++ JavaBasic/Arrays/challenge of chatgpt/Test.java
@@ -20,10 +20,75 @@ public class Test {
         }
         System.out.print(secondLargest);
     }
-    public static void main(String[] args){
-        int number[] = {3, 4, 6, 8 ,1, 7};
-        largestOfTwo(number);
 
+
+    //----------------------------------------------------------------------------------------------------------------------------------
+    //Even/Odd
+
+
+
+    public static void oddEven(int number[]){
+
+    int even = 0;
+    int odd = 0;
+
+        for(int i = 0; i < number.length; i++){
+            if(number[i] % 2 == 0){
+                even++;
+            }else{
+                odd++;
+            }
+        }
+
+        System.out.println("Even : " + even);
+        System.out.println("Odd : " + odd);
+    }
+
+
+    //----------------------------------------------------------------------------------------------------------------------------------
+    //Check sorted
+
+    public static boolean checkSorted(int number[]){
+
+        for (int i = 0; i < number.length - 1; i++){
+            if(number[i] > number[i + 1]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    //----------------------------------------------------------------------------------------------------------------------------------
+    //misssing number
+
+    public static void missingNumber(int number[]){
+
+        for(int i = 0; i < number.length; i++){
+            if(number[i] != i+1){
+                System.out.print("Missing number is : " + (i+1));
+                return;
+            }
+        }
+    }
+
+
+
+    
+    public static void main(String[] args){
+        // int number[] = {3, 4, 6, 8 ,1, 7};
+        // largestOfTwo(number);
+
+
+        // int number[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        // oddEven(number);
+
+        // int number[] = {1, 2, 3, 4, 5, 6,7,  8, 9};
+        // System.out.println(checkSorted(number)); 
+
+        int number[] = {1, 2, 3, 5, 6, 7, 8, 9};
+        missingNumber(number); 
         
+
     }
 }
